ec62198e1b2d0677c4af867f3e22917c9c60dfd4
dotzero <mail@dotzero.ru>
2021-04-05 08:40:46 +0300

    Update adminer to 4.8.0

--- src/AdminerController.php
+++ src/AdminerController.php
@@ -6,7 +6,7 @@ class AdminerController extends Controller {
 
     public function index()
     {
-        require('adminer-4.7.0-en.php');
+        require('adminer-4.8.0-en.php');
         return new EmptyResponse();
     }
 
